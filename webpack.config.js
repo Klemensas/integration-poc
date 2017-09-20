65ade5cb09b6c416ee135e0a442418446a6507d1
Klemensas <hello@klemensas.net>
2017-09-20 21:45:11 +0300

    ts, webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'build'),  
+        filename: 'bundle.js',
+        publicPath: '/build/'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    externals: {
+      "Dropbox": "dropbox"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        inject: true,
+      }),
+    ],
+}
